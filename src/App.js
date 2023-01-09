da604cd5a9e08010e68b5d879c037175b2ed5d4a
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2023-01-09 18:33:08 +0500

    example-pagination

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      < Navbar />
+    </div>
+  );
+}
+
+export default App;
